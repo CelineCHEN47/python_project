fdd42ebcea22fd263865e39748ab5e23c0e46de4
Yuhan Chen <3123738092@qq.com>
2025-11-18 22:10:44 +0800

    Initial try, just use 300 rows of data to debug

--- check.py
+++
@@ -1,4 +0,0 @@
-from evaluate import load
-rouge = load("rouge")
-print(rouge.compute(predictions=["hello world"], references=["hello world"]))
-# 应输出类似 {'rouge1': ..., 'rouge2': ..., 'rougeL': ...}
